3ec70dc3dc73e0df0d9ff2ac1fa77ecd86268da0
Anass El Achham <anas.elachham@gmail.com>
2025-03-10 12:33:55 +0000

    Update poetry.lock and ensure Python 3.12 compatibility

--- build.sh
+++ build.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-# Script de déploiement pour Render
-
-# Installer Poetry
 pip install poetry
-
-# Installer les dépendances (sans le groupe dev)
 poetry install --without dev --no-interaction --no-ansi
-
-# Vérifier si l'installation a réussi
 if [ $? -ne 0 ]; then
     echo "Erreur : l'installation des dépendances a échoué."
     exit 1
 fi
-
-# Préparation finale
 echo "Préparation terminée"
