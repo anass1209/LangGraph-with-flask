6cd4a5ab8f6fdc7fd5fa77f3b36d12dc37f5f2a7
anass el-achham <121577030+anass1209@users.noreply.github.com>
2025-03-10 12:35:51 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+# Script de déploiement pour Render
+
+# Installer Poetry
 pip install poetry
-poetry install --without dev --no-interaction --no-ansi
+
+# Installer les dépendances (sans le groupe dev, sans installer le projet)
+poetry install --without dev --no-root --no-interaction --no-ansi
+
+# Vérifier si l'installation a réussi
 if [ $? -ne 0 ]; then
     echo "Erreur : l'installation des dépendances a échoué."
     exit 1
 fi
+
+# Préparation finale
 echo "Préparation terminée"
